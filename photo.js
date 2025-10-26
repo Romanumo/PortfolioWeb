d2c92425889889eca6638e3b043dbb4cfd7ff6a2
Roman <romanumo1@gmail.com>
2025-10-25 22:12:14 -0700

    MorePhotos

--- photo.js
+++ photo.js
@@ -1,6 +1,6 @@
 const mainGallery = document.getElementById("mainGallery");
 
-for(let i = 1; i < 15;i++)
+for(let i = 1; i < 18;i++)
 {
     const img = document.createElement("img");
     img.src = `Photography/${i}.jpg`;
